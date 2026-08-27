0803334966a71f5bc388b759ca852b3d072d7ccf
EvgenijPM <evgenij.gulyaev.92gulyaeff@yandex.ru>
2026-08-27 18:02:13 +0700

    Fix: add videos to git and remove turbopack

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['better-sqlite3'],
-  turbopack: {},
   experimental: {
     proxyClientMaxBodySize: '1000mb',
   },
