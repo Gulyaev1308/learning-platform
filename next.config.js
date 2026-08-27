673e3bf02cb1c8009f6c06951b98102a0b089748
EvgenijPM <evgenij.gulyaev.92gulyaeff@yandex.ru>
2026-08-27 18:34:54 +0700

    Increase body size limit to 500MB

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['better-sqlite3'],
+  experimental: {
+    proxyClientMaxBodySize: '500mb',
+  },
 }
 
 module.exports = nextConfig
